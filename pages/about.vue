705b7fc2331a215fec810b9f0cf476c00e5645f2
GetALife <1340482017@qq.com>
2020-06-16 18:08:28 +0800

    fix style

--- pages/about.vue
+++ pages/about.vue
@@ -28,7 +28,7 @@ export default {
 
 <style scoped>
 .container {
-  height: 100vh;
+  min-height: 100vh;
   width: 100vw;
   padding: 40px;
   display: flex;
--- pages/index.vue
+++ pages/index.vue
@@ -30,7 +30,7 @@ export default {}
 
 <style scoped>
 .root {
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
